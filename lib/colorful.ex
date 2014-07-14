0d7b41a1d5190a84db50966cfe137333ab48b6dc
Joe-noh <goflb.jh@gmail.com>
2014-07-14 23:15:27 +0900

    Colorful.string works

--- lib/colorful.ex
+++ lib/colorful.ex
@@ -1,2 +1,18 @@
 defmodule Colorful do
+  defmacro __using__(_) do
+    quote do
+      require Colorful
+    end
+  end
+
+  defmacro string(string, colors) do
+    decos = Atom.to_string(colors) |> String.split("_")
+    quote do
+      unquote(
+        List.foldl decos, "", fn(deco, acc) ->
+          quote do: unquote(acc) <> IO.ANSI.unquote(String.to_atom deco)
+        end
+      ) <> unquote(string) <> IO.ANSI.reset
+    end
+  end
 end
--- test/colorful_test.exs
+++ test/colorful_test.exs
@@ -1,7 +1,12 @@
 defmodule ColorfulTest do
   use ExUnit.Case
+  use Colorful
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Colorful, as: C
+
+  test :string do
+    assert C.string("hello", :red) == "\e[31mhello\e[0m"
+    assert C.string("hello", :red_underline) == "\e[31m\e[4mhello\e[0m"
+    assert C.string("hello", :underline_red) == "\e[4m\e[31mhello\e[0m"
   end
 end
