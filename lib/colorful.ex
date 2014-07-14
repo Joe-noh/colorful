5dfbae5239b6b853b9b7d0a14cd8aa99c3d9b558
Joe-noh <goflb.jh@gmail.com>
2014-07-15 01:32:17 +0900

    add puts and inspects
    default decorator is :reset
    either atom decorator or string decorator is adopted

--- lib/colorful.ex
+++ lib/colorful.ex
@@ -5,14 +5,41 @@ defmodule Colorful do
     end
   end
 
-  defmacro string(string, colors) do
-    decos = Atom.to_string(colors) |> String.split("_")
+  defmacro string(target, decorators \\ :reset) do
     quote do
       unquote(
-        List.foldl decos, "", fn(deco, acc) ->
+        List.foldl split_decorators(decorators), "", fn(deco, acc) ->
           quote do: unquote(acc) <> IO.ANSI.unquote(String.to_atom deco)
         end
-      ) <> unquote(string) <> IO.ANSI.reset
+      ) <> unquote(target) <> IO.ANSI.reset
     end
+  end
+
+  defmacro puts(target, decorators \\ :reset) do
+    quote do
+      IO.puts(unquote(
+        List.foldl split_decorators(decorators), "", fn(deco, acc) ->
+          quote do: unquote(acc) <> IO.ANSI.unquote(String.to_atom deco)
+        end
+      ) <> unquote(target) <> IO.ANSI.reset)
+    end
+  end
+
+  defmacro inspects(target, decorators \\ :reset) do
+    quote do
+      IO.puts(unquote(
+        List.foldl split_decorators(decorators), "", fn(deco, acc) ->
+          quote do: unquote(acc) <> IO.ANSI.unquote(String.to_atom deco)
+        end
+      ) <> (inspect unquote(target)) <> IO.ANSI.reset)
+    end
+  end
+
+  defp split_decorators(decorators) when is_atom(decorators) do
+    Atom.to_string(decorators) |> String.split("_")
+  end
+
+  defp split_decorators(decorators) when is_binary(decorators) do
+    decorators |> String.split("_")
   end
 end
