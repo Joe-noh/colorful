9d34f2fd5cf9fb927c3e932ae7c02495cab570e7
Joe-noh <goflb.jh@gmail.com>
2014-07-15 02:03:48 +0900

    add docs

--- lib/colorful.ex
+++ lib/colorful.ex
@@ -1,10 +1,23 @@
 defmodule Colorful do
+  @moduledoc """
+    Wrapper for IO.ANSI module.
+
+        use Colorful
+
+        Colorful.string("hello", "red white_background bright")
+
+        Colorful.puts("hello", "red")
+        Colorful.inspects(:hello, "underline")
+  """
+
+  @doc false
   defmacro __using__(_) do
     quote do
       require Colorful
     end
   end
 
+  @doc "Make colored string"
   defmacro string(target, decorators \\ "reset") do
     quote do
       unquote(
@@ -15,6 +28,7 @@ defmodule Colorful do
     end
   end
 
+  @doc "Output colored string to stdout"
   defmacro puts(target, decorators \\ "reset") do
     quote do
       IO.puts(unquote(
@@ -25,6 +39,7 @@ defmodule Colorful do
     end
   end
 
+  @doc "Note that inspects, not inspect"
   defmacro inspects(target, decorators \\ "reset") do
     quote do
       IO.puts(unquote(
@@ -35,6 +50,7 @@ defmodule Colorful do
     end
   end
 
+  @doc false
   defp split_decorators(decorators) when is_binary(decorators) do
     decorators |> String.split(" ", trim: true)
   end
