4d84fa9edeefbf624c58dc8e8667119ce9890881
Joe-noh <goflb.jh@gmail.com>
2014-10-16 11:35:30 +0900

    prepend IO.ANSI.reset not only append
    with that, change default decorator from "reset" to nil

--- test/colorful_test.exs
+++ test/colorful_test.exs
@@ -7,31 +7,31 @@ defmodule ColorfulTest do
   import ExUnit.CaptureIO
 
   test :string do
-    assert C.string("hello", "  red  ")       == "\e[31mhello\e[0m"
-    assert C.string("hello", "red underline") == "\e[31m\e[4mhello\e[0m"
-    assert C.string("hello", "underline red") == "\e[4m\e[31mhello\e[0m"
+    assert C.string("hello", "  red  ")       == "\e[0m\e[31mhello\e[0m"
+    assert C.string("hello", "red underline") == "\e[0m\e[31m\e[4mhello\e[0m"
+    assert C.string("hello", "underline red") == "\e[0m\e[4m\e[31mhello\e[0m"
   end
 
   test :puts do
-    assert capture_io(fn -> C.puts("hello", :red) end) == "\e[31mhello\e[0m\n"
+    assert capture_io(fn -> C.puts("hello", :red) end) == "\e[0m\e[31mhello\e[0m\n"
   end
 
   test :puts_to_device do
     assert capture_io(:stderr, fn ->
       C.puts(:stderr, "hello", "red")
-    end) == "\e[31mhello\e[0m\n"
+    end) == "\e[0m\e[31mhello\e[0m\n"
 
     assert capture_io(:stderr, fn -> C.puts("hello", "red") end) == ""
   end
 
   test :inspect do
-    assert capture_io(fn -> C.inspect([1, 2], "red") end) == "\e[31m[1, 2]\e[0m\n"
+    assert capture_io(fn -> C.inspect([1, 2], "red") end) == "\e[0m\e[31m[1, 2]\e[0m\n"
   end
 
   test :inspect_to_device do
     assert capture_io(:stderr, fn ->
       C.inspect(:stderr, [1, 2], "red")
-    end) == "\e[31m[1, 2]\e[0m\n"
+    end) == "\e[0m\e[31m[1, 2]\e[0m\n"
 
     assert capture_io(:stderr, fn -> C.inspect([1, 2], "red") end) == ""
   end
@@ -45,8 +45,8 @@ defmodule ColorfulTest do
   end
 
   test :adapt_a_list do
-    assert C.string("hello", ["underline", "red"]) == "\e[4m\e[31mhello\e[0m"
-    assert C.string("hello", [:underline,  "red"]) == "\e[4m\e[31mhello\e[0m"
+    assert C.string("hello", ["underline", "red"]) == "\e[0m\e[4m\e[31mhello\e[0m"
+    assert C.string("hello", [:underline,  "red"]) == "\e[0m\e[4m\e[31mhello\e[0m"
   end
 
   test :raise_argument_error do
